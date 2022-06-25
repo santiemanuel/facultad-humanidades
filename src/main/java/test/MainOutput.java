74fd37bd87bd702f5ce72e9cd8bcd80e0599bb68
santiemanuel <santiemanuel@gmail.com>
2022-06-25 20:59:22 -0300

    Cargue una carrera a la facultad en main

--- src/main/java/test/MainOutput.java
+++ src/main/java/test/MainOutput.java
@@ -1,12 +1,16 @@
 package test;
 
+import com.milprogramadores.model.Carrera;
 import com.milprogramadores.model.Facultad;
 
 public class MainOutput {
 
 	public static void main(String[] args) {
 		Facultad facultad = new Facultad();
+		
 		facultad.setNombre("Humanidades");
+		
+		facultad.agregarCarrera(new Carrera());
 		
 		System.out.println(facultad);
 	}
