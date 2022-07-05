e479f86f3a3bdbb4916753453c801e94931f037e
santiemanuel <santiemanuel@gmail.com>
2022-07-05 15:37:53 -0300

    Correccion de lineas en el DAO usuario

--- src/main/java/com/milprogramadores/dao/UsuarioDAO.java
+++ src/main/java/com/milprogramadores/dao/UsuarioDAO.java
@@ -14,7 +14,7 @@ import com.milprogramadores.sql.DbConnection;
 
  public class UsuarioDAO {
 
-   private final String INSERT_USUARIO  = "INSERT INTO usuarios VALUES ( defaul, ?, ?)";
+   private final String INSERT_USUARIO  = "INSERT INTO usuarios VALUES ( default, ?, ?)";
    private final String DELETE_USUARIO  = "DELETE FROM usuarios WHERE id_usuario = ?"; 
    private final String UPDATE_USUARIO  = "UPDATE usuarios SET email_usuario = ?, rol_admin = ? WHERE id_usuario = ?";
    private final String GET_ONE_USUARIO = "SELECT * FROM usuarios WHERE usuario_id = ?";
@@ -22,80 +22,73 @@ import com.milprogramadores.sql.DbConnection;
 	
 		
     public UsuarioDAO(){
-	 
-  }
+ 
+    }
    
     public void agregarUsuario(Usuario usuario) {
     	DbConnection conn = new DbConnection();
     	
-    try {
-    	PreparedStatement pstmt = conn.getConnection().prepareStatement(INSERT_USUARIO);
+    	try {
+    		PreparedStatement pstmt = conn.getConnection().prepareStatement(INSERT_USUARIO);
     
-    	pstmt.setString(1, usuario.getEmail_usuario());
-    	pstmt.setBoolean(2, usuario.getRol_admin());
+    		pstmt.setString(1, usuario.getEmail_usuario());
+    		pstmt.setBoolean(2, usuario.getRol_admin());
     	
-    	pstmt.executeUpdate();
-    	pstmt.close();
-    	conn.disconnect();
-    } catch (SQLException e) {
-      e.printStackTrace();	
-     }
-      	
-  }
+    		pstmt.executeUpdate();
+    		pstmt.close();
+    		conn.disconnect();
+    	} catch (SQLException e) {
+    		e.printStackTrace();	
+    	}
+    }
   
-   public void eliminarUsuario(int id) {
-	   
-	DbConnection conn = new DbConnection();
+    public void eliminarUsuario(int id) {
+    	DbConnection conn = new DbConnection();
 	
-	 try {
-		PreparedStatement pstmt = conn.getConnection().prepareStatement(DELETE_USUARIO);
-		pstmt.setInt(1, id);
-		pstmt.executeUpdate();
-		pstmt.close();
-		conn.disconnect();
-	 } catch(SQLException e) {
+    	try {
+    		PreparedStatement pstmt = conn.getConnection().prepareStatement(DELETE_USUARIO);
+    		pstmt.setInt(1, id);
+    		pstmt.executeUpdate();
+    		pstmt.close();
+    		conn.disconnect();
+    	} catch(SQLException e) {
 		 e.printStackTrace(); 
-	 }	 
-	 
+    	}
    }
    
    public void actualizarUsuario(Usuario usuario) {
-	 DbConnection conn = new DbConnection();
+	   DbConnection conn = new DbConnection();
 	 
-	 try {
-		PreparedStatement pstmt = conn.getConnection().prepareStatement(UPDATE_USUARIO); 
-		 
-		pstmt.setString(1, usuario.getEmail_usuario());
-		pstmt.setBoolean(2, usuario.getRol_admin());
-		pstmt.setInt(3, usuario.getId_usuario());
+	   try {
+		   PreparedStatement pstmt = conn.getConnection().prepareStatement(UPDATE_USUARIO); 
+		   pstmt.setString(1, usuario.getEmail_usuario());
+		   pstmt.setBoolean(2, usuario.getRol_admin());
+		   pstmt.setInt(3, usuario.getId_usuario());
 		
-		pstmt.executeUpdate();
-		pstmt.close();
-		conn.disconnect();
-	 } catch (SQLException e) {
+		   pstmt.executeUpdate();
+		   pstmt.close();
+		   conn.disconnect();
+	   } catch (SQLException e) {
 		e.printStackTrace(); 
-	 }	 
+	   }	 
    }
    
    public ArrayList<Usuario> listarUsuarios(){
-	ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+	   ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+	   DbConnection conn = new DbConnection();
 	
-	DbConnection conn = new DbConnection();
-	
-	try {
-	  PreparedStatement pstmt = conn.getConnection().prepareStatement(GET_ALL_USUARIO);
-	  pstmt.execute();
+	   try {
+		   PreparedStatement pstmt = conn.getConnection().prepareStatement(GET_ALL_USUARIO);
+		   pstmt.execute();
 	  
-	  ResultSet rs = pstmt.getResultSet();
+		   ResultSet rs = pstmt.getResultSet();
 	  
-	  
-	  while(rs.next()) {
-		Usuario usuario = new Usuario();  
-		  
-		usuario.setId_usuario(rs.getInt("id_usuario"));
-		usuario.setEmail_usuario(rs.getString("email_usuario"));
-		usuario.setRol_admin(rs.getBoolean("rol_admin"));
-		usuarios.add(usuario);  
+		   while(rs.next()) {
+			   Usuario usuario = new Usuario();  
+			   usuario.setId_usuario(rs.getInt("id_usuario"));
+			   usuario.setEmail_usuario(rs.getString("email_usuario"));
+			   usuario.setRol_admin(rs.getBoolean("rol_admin"));
+			   usuarios.add(usuario);  
 	  }
 	  rs.close();
 	  pstmt.close();
@@ -133,9 +126,6 @@ import com.milprogramadores.sql.DbConnection;
     }catch(SQLException e) {
         e.printStackTrace();	
     }
-    return null;	 	
- 
-   }
-  
+    return null;
+   } 
 }
-
